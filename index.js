4baeb131596efddee9cff28fa68daa49eb234c36
Chris Brown <chris.brown@skookum.com>
2017-09-14 09:36:23 -0600

    ✨ async/await

--- index.js
+++ index.js
@@ -35,23 +35,19 @@ const createDatabase = (database = {
 
 const db = createDatabase();
 
-// the key thing to notice here is that you can flatten out your promises by using Promise.all to pass down the results of each step down to the next block.
-const user = db.getUsers(1)
-.then(users => Promise.all([
-  users[0],                           // user
-  db.getWorkoutsByUserId(users[0].id) // workouts
-]))
-.then(([user, workouts]) => Promise.all([
-  Object.assign({}, user, { workouts }),                                   // userWithWorkouts
-  Promise.all(workouts.map(workout => db.getLiftsByWorkoutId(workout.id))) // workoutLifts
-]))
-.then(([userWithWorkouts, workoutLifts]) => {
-  const workouts = userWithWorkouts.workouts.map(workout => {
-    const lifts = workoutLifts.find(lifts => lifts.some(lift => lift.workout_id === workout.id));
+const getUser = async function getUser(id) {
+  const [ user ] = await db.getUsers(id);
+  const workouts = await db.getWorkoutsByUserId(user.id);
+  const allLifts = await Promise.all(workouts.map(workout => db.getLiftsByWorkoutId(workout.id)));
+
+  const workoutsWithLifts = workouts.map(workout => {
+    const lifts = allLifts.find(lifts => lifts.some(lift => lift.workout_id === workout.id));
     return Object.assign({}, workout, { lifts });
   });
 
-  return Object.assign({}, userWithWorkouts, { workouts });
-});
+  return Object.assign( {}, user, { workouts: workoutsWithLifts });
+};
 
-user.then(result => { console.log(JSON.stringify(result)) });
+getUser(1).then(user => {
+  console.log(JSON.stringify(user));
+})
